d225f96f5c636e619a3bf878ff83e9143d6c3156
Lucas Dutra <ldutra98@gmail.com>
2022-09-21 01:53:30 -0300

    Create delay support function

---
+++ src/delay.c
@@ -0,0 +1,11 @@
+#include "delay.h"
+
+void delay(int number_of_seconds)
+{
+    int milli_seconds = 1000 * number_of_seconds;
+
+    clock_t start_time = clock();
+
+    while (clock() < start_time + milli_seconds)
+        ;
+}
